950d7ce7e4e7fef4ef309b5c3e5dba63056ee60f
polarctos <polarctos@users.noreply.github.com>
2020-12-22 18:11:22 +0100

    Add usage of Argon2 as a key derivation function

--- pkg/vault/pass.go
+++ pkg/vault/pass.go
@@ -2,13 +2,13 @@ package vault
 
 import (
 	"crypto/sha256"
+	"golang.org/x/crypto/argon2"
 	"golang.org/x/crypto/pbkdf2"
 )
 
 const (
-	keyLength  = 32    // for AES-256
-	ivLength   = 12    // common for AES-GCM
-	iterations = 10000 // reasonable default of OpenSSL
+	keyLength = 32 // for AES-256
+	ivLength  = 12 // common for AES-GCM
 )
 
 type key struct {
@@ -18,11 +18,25 @@ type key struct {
 
 // derive key using PBKDF2 with SHA-256
 func deriveKey(password []byte, salt []byte) *key {
-	kdfResult := pbkdf2.Key(password, salt, iterations, keyLength+ivLength, sha256.New)
-	return &key{
-		aesKey: kdfResult[:keyLength],
-		iv:     kdfResult[keyLength : keyLength+ivLength],
-	}
+	// reasonable defaults used by OpenSSL
+	var hash = sha256.New
+	var iterations = 10000
+	kdfResult := pbkdf2.Key(password, salt, iterations, keyLength+ivLength, hash)
+	return splitKdfResult(kdfResult)
 }
 
+func deriveKeyArgon2id(password []byte, salt []byte) *key {
+	// recommended values from the RFC and of the golang documentation
+	var time uint32 = 1
+	var memory uint32 = 64 * 1024
+	var threads uint8 = 4
+	kdfResult := argon2.IDKey(password, salt, time, memory, threads, keyLength+ivLength)
+	return splitKdfResult(kdfResult)
+}
 
+func splitKdfResult(k []byte) *key {
+	return &key{
+		aesKey: k[:keyLength],
+		iv:     k[keyLength : keyLength+ivLength],
+	}
+}
