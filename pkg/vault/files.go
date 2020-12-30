5b6319597e1b8925c1ce89627c267fc1d6f268a3
polarctos <polarctos@users.noreply.github.com>
2020-12-30 20:35:14 +0100

    Support relative and user home directory file paths

--- pkg/vault/files.go
+++ pkg/vault/files.go
@@ -3,6 +3,7 @@ package vault
 import (
 	"io/ioutil"
 	"net/url"
+	"os"
 	"strings"
 )
 
@@ -19,16 +20,39 @@ func maybeFromFiles(s1 string, s2 string) (v1 string, v2 string, err error) {
 }
 
 func maybeFromFile(s string) (string, error) {
-	if strings.HasPrefix(s, "file:/") {
-		uri, err := url.ParseRequestURI(s)
+	if !strings.HasPrefix(s, "file:/") {
+		return s, nil
+	} else {
+		path, err := absoluteFilePath(s)
 		if err != nil {
 			return "", err
 		}
-		file, err := ioutil.ReadFile(uri.Path)
+		file, err := ioutil.ReadFile(path)
 		if err != nil {
 			return "", err
 		}
 		return string(file), nil
 	}
-	return s, nil
+}
+
+func absoluteFilePath(s string) (path string, err error) {
+	// support relative paths and user directory:
+	if strings.HasPrefix(s, "file://.") {
+		return s[len("file://"):], nil
+	} else if strings.HasPrefix(s, "file://~") {
+		userHomeDir, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		} else {
+			return userHomeDir + s[len("file://")+1:], nil
+		}
+	} else {
+		// other paths:
+		uri, err := url.ParseRequestURI(s)
+		if err == nil {
+			return uri.Path, nil
+		} else {
+			return "", err
+		}
+	}
 }
--- pkg/vault/files_test.go
+++ pkg/vault/files_test.go
@@ -3,6 +3,7 @@ package vault
 import (
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,8 @@ import (
 func Test_maybeFromFiles(t *testing.T) {
 	f1 := testFile(t, "test-file-1-", "test-pw")
 	f2 := testFile(t, "test-file-2-", "test-data")
+	defer deleteTestFile(f1)
+	defer deleteTestFile(f2)
 
 	v1, v2, err := maybeFromFiles("file://"+f1.Name(), "file://"+f2.Name())
 	assert.Nil(t, err)
@@ -20,6 +23,7 @@ func Test_maybeFromFiles(t *testing.T) {
 
 func Test_maybeFromFile(t *testing.T) {
 	f := testFile(t, "test-file-x-", "test")
+	defer deleteTestFile(f)
 
 	prefixes := []string{"file://", "file://localhost"}
 	for _, prefix := range prefixes {
@@ -48,4 +52,28 @@ func testFile(t *testing.T, name string, content string) *os.File {
 	_, err = f.WriteString(content)
 	assert.Nil(t, err)
 	return f
+}
+
+func deleteTestFile(file *os.File) {
+	err := os.Remove(file.Name())
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Test_absoluteFilePaths(t *testing.T) {
+	v, err := absoluteFilePath("file:///var/test/file.txt")
+	assert.Nil(t, err, "absolute path")
+	assert.Equal(t, "/var/test/file.txt", v)
+
+	v, err = absoluteFilePath("file://./var/test/file.txt")
+	assert.Nil(t, err, "relative path")
+	assert.Equal(t, "./var/test/file.txt", v)
+
+	v, err = absoluteFilePath("file://~/var/test/file.txt")
+	assert.Nil(t, err, "user directory path")
+	assert.True(t, strings.HasSuffix(v, "/var/test/file.txt"))
+	assert.False(t, strings.HasSuffix(v, "~/var/test/file.txt"))
+	userHomeDir, _ := os.UserHomeDir()
+	assert.Equal(t, userHomeDir+"/var/test/file.txt", v)
 }
