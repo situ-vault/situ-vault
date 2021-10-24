2906c9be19178ce1255f482c46c4641a68c00a90
polarctos <polarctos@users.noreply.github.com>
2021-10-25 00:40:30 +0200

    Nicer input boxes with easier background

--- gui/situ-vault/main.go
+++ gui/situ-vault/main.go
@@ -9,6 +9,7 @@ var driver fyne.Driver
 
 func main() {
 	application := app.New()
+	application.Settings().SetTheme(&fyneTheme{})
 	driver = application.Driver()
 	exp := newExperience()
 	exp.loadUi(application)
